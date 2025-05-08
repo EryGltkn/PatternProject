39cd4ba4ac362d964454c885052ad8c298fb360d
EryGltkn <erygltkn817104@gmail.com>
2025-05-08 13:19:19 +0300

    IteratorsFinished

---
+++ src/WindyCityIterator.java
@@ -0,0 +1,34 @@
+public class WindyCityIterator implements CityIterator {
+    private Cities citiesInstance;
+    private City[] cityList;
+    private int currentIndex = 0;
+
+    public WindyCityIterator() {
+        citiesInstance = new Cities().GetCitiesInstance();
+        cityList = citiesInstance.getCityList();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (currentIndex < cityList.length) {
+            if (cityList[currentIndex].getCurrentWeatherCondition().equals("Windy")) {
+                return true;
+            }
+            currentIndex++;
+        }
+        return false;
+    }
+
+    @Override
+    public City next() {
+        if (hasNext()) {
+            return cityList[currentIndex++];
+        }
+        return null; // or throw an exception
+    }
+
+    @Override
+    public void reset() {
+        currentIndex = 0;
+    }
+}
