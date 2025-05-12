97cbac4beeef0ffaa512e5c93ba7b4ad72ae9874
EryGltkn <erygltkn817104@gmail.com>
2025-05-12 20:24:04 +0300

    Seperate Thread Randomizer
    Updating randomizer added

---
+++ CityRandomizer.java
@@ -0,0 +1,22 @@
+public class CityRandomizer {
+    public static void StartRandomizerThread() {
+        Cities citiesInstance = Cities.GetCitiesInstance();
+        City[] cityList = citiesInstance.getCityList();
+        
+        Thread randomizerThread = new Thread(() -> {
+            while (true) {
+                try {
+                    for (City city : cityList) {
+                        city.randomizeWeatherCondition();
+                        city.randomizeTemperature();
+                    }
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    System.out.println("Randomizer thread interrupted");
+                    break;
+                }
+            }
+        });
+        randomizerThread.start();
+    }
+}
